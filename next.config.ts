08f278e2aaafad9344941cadadbf7d6518a2e205
Steven Nguyen <stevenewhen@gmail.com>
2025-10-22 12:44:07 -0500

    Enable static export

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,6 @@
 import type { NextConfig } from 'next';
-
 const nextConfig: NextConfig = {
-  // create a fully static site in /out
   output: 'export',
-  // avoids next/image processing during static export
   images: { unoptimized: true },
-  // optional: if you rely on absolute links ending with /
-  // trailingSlash: true,
 };
-
 export default nextConfig;
